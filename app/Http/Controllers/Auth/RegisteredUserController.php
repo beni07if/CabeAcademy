bcf42a776c66eff268529671d1effcf0bceef086
Beni <beni07if@gmail.com>
2023-06-20 11:46:41 +0700

    dashboardsss

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -32,12 +32,13 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'unit_id' => $request->unit_id,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
