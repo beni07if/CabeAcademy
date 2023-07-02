0b82f2fc12b43d58015c8c7699137ca4a9126f71
beni07if <beni07elreal@gmail.com>
2023-07-02 17:44:21 +0700

    update 5

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,24 @@
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: [
+//                 'resources/css/app.css',
+//                 'resources/js/app.js',
+//             ],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
-            refresh: true,
-        }),
-    ],
+  build: {
+    publicDir: '/home/beni07/public_html/repositories/CabeAcademy/public/build',
+  },
 });
+
