b5ac1e7440d2168c54492a3d288a5a8e4847bab5
beni07if <beni07elreal@gmail.com>
2023-07-02 19:49:28 +0700

    update 9

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,166 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<!DOCTYPE html>
+<html lang="en">
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+  <title>Login / Cabe Academy</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+  <!-- Favicons -->
+  <link href="{{asset ('NiceAdmin/assets/img/favicon.png') }}" rel="icon">
+  <link href="{{asset ('NiceAdmin/assets/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
+  <!-- Vendor CSS Files -->
+  <link href="{{asset ('NiceAdmin/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/quill/quill.snow.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/quill/quill.bubble.css') }}" rel="stylesheet">
+  <link href="{{asset ('NiceAdmin/assets/vendor/simple-datatables/style.css') }}" rel="stylesheet">
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+  <!-- Template Main CSS File -->
+  <link href="{{asset ('NiceAdmin/assets/css/style.css') }}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: 'NiceAdmin - v2.1.0
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <main>
+    <div class="container">
+
+      <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+        <div class="container">
+          <div class="row justify-content-center">
+            <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+
+              <div class="d-flex justify-content-center py-4">
+                <a href="index.html" class="logo d-flex align-items-center w-auto">
+                  <!-- <img src="{{asset('Arsha/assets/img/logo/png/logo-black.png')}}" alt=""> -->
+                  <!-- <span class="d-none d-lg-block">'NiceAdmin</span> -->
                 </a>
-            @endif
+              </div><!-- End Logo -->
 
-            <x-primary-button class="ml-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
+              <div class="card mb-3">
+
+                <div class="card-body">
+
+                  <div class="pt-4 pb-2">
+                    <h5 class="card-title text-center pb-0 fs-4">Login to Dashboard ID Academy</h5>
+                    <p class="text-center small">Enter your unit/topic & password to login</p>
+                  </div>
+
+                  <form class="row g-3 needs-validation" novalidate method="POST" action="{{ route('login') }}">
+        @csrf
+        
+                    <label for="email" class="form-label">Email</label>
+                    <div class="input-group has-validation">
+                        <select name="email" class="form-select" id="email" required>
+                            <option value="beni07if@gmail.com">beni07if@gmail.com</option>
+                            <option value="Option 2">Option 2</option>
+                            <option value="Option 3">Option 3</option>
+                        </select>
+                        <div class="invalid-feedback">Please select an option or enter your username.</div>
+                    </div>
+
+                    <div class="mt-4">
+                        <label for="password" class="form-label">{{ __('Password') }}</label>
+                        <input type="password" name="password" class="form-control" id="password" required autocomplete="current-password">
+                        <div class="invalid-feedback">Please enter your password!</div>
+                    </div>
+
+
+
+                    <!-- Email Address -->
+                    <!-- <div>
+                        <x-input-label for="email" :value="__('Email')" />
+                        <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div> -->
+
+                    <!-- Password -->
+                    <!-- <div class="mt-4">
+                        <x-input-label for="password" :value="__('Password')" />
+
+                        <x-text-input id="password" class="block mt-1 w-full"
+                                        type="password"
+                                        name="password"
+                                        required autocomplete="current-password" />
+
+                        <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                    </div> -->
+
+                    <!-- Remember Me -->
+                    <!-- <div class="block mt-4">
+                        <label for="remember_me" class="inline-flex items-center">
+                            <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                    </div> -->
+
+                    <div class="flex items-center justify-end mt-4">
+                        <!-- @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif -->
+
+                        <div class="col-12">
+                            <button class="btn btn-primary w-100" type="submit">Login</button>
+                        </div>
+
+                    </div>
     </form>
-</x-guest-layout>
+
+                </div>
+              </div>
+
+              <div class="credits">
+                <!-- All the links in the footer should remain intact. -->
+                <!-- You can delete the links only if you purchased the pro version. -->
+                <!-- Licensing information: https://bootstrapmade.com/license/ -->
+                <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
+                INOVASI DIGITAL <a href="https://bootstrapmade.com/"></a>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+      </section>
+
+    </div>
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset ('NiceAdmin/assets/vendor/bootstrap/js/bootstrap.bundle.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/quill/quill.min.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/tinymce/tinymce.min.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/simple-datatables/simple-datatables.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/chart.js') }}/chart.min.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/apexcharts/apexcharts.min.js') }}"></script>
+  <script src="{{asset ('NiceAdmin/assets/vendor/echarts/echarts.min.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{asset ('NiceAdmin/assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
